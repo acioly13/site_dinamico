8ec37a10ed05a9c38f892d146c5dc0208a7580a3
acioly <joao.acioly@dmslog.com>
2022-03-28 17:33:09 -0300

    Seção 3 inicio

--- simplemooc/urls.py
+++ simplemooc/urls.py
@@ -18,7 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('simplemooc.core.urls'))
+    path('', include('simplemooc.core.urls')),
+    path('cursos/', include('simplemooc.courses.urls'))
 
 
 ]
